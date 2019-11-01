1a9b1ce5cd7352daf93310caa4f72bf8fc1b8906
ayehninwai21 <ayehninwai21@gmail.com>
2019-11-01 15:19:40 +0630

    a few change in prefSetting

--- app/src/main/java/com/coder/mm/mypraticeapplication/MainActivity.kt
+++ app/src/main/java/com/coder/mm/mypraticeapplication/MainActivity.kt
@@ -6,23 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnSetting.setOnClickListener {
-            val intent = Intent(this@LoginActivity, SettingActivity::class.java)
+            val intent = Intent(this@MainActivity, SettingActivity::class.java)
             startActivity(intent)
         }
       val fm=supportFragmentManager
         fm.beginTransaction().add(R.id.fragLinear,FragOne(),"frag_one").commit()
         btnGetPref.setOnClickListener {
-            var cd_firm = getPrefValue(this@LoginActivity, "cd_key")
-            var ip = getPrefValue(this@LoginActivity, "IP")
-            Log.d("my_message", "cd_firm is $cd_firm and ip is $ip and sub1 is" )
+            var key1 = getPrefValue(this@MainActivity, "key1")
+            var key2 = getPrefValue(this@MainActivity, "key2")
+            Log.d("my_message", "cd_firm is $key1 and ip is $key2 and sub1 is" )
+        Toast.makeText(this@MainActivity,"key one is $key1 and key two is $key2",Toast.LENGTH_LONG).show()
+
         }
 
     }
